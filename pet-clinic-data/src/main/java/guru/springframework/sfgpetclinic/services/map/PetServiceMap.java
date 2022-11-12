e743f61b116a43fc3fa8f997df0a9bb7e3562287
rameezalam <rameez.gce@gmail.com>
2022-11-12 23:11:13 +0530

    adding spring config

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetServiceMap.java
@@ -8,6 +8,7 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.util.Set;
 
+
 public class PetServiceMap extends AbstractMapService<Pet,Long> implements PetService {
     @Override
     public Set<Pet> findAll() {
